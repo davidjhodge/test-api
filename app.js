52a54ced019880d6ae3221da03faa1e0edbcf147
davidjhodge <dhodge416@gmail.com>
2017-08-16 01:04:32 -0400

    Add Partially Functioning Comments Endpoint
    /comments now returns a placeholder value for results_count and an empty
    result for the comments.

--- app.js
+++ app.js
@@ -3,12 +3,31 @@
 const express = require('express')
 const app = express()
 
+// Connect Database
+const { Client } = require('pg')
+
+const client = new Client({
+  user: 'davidhodge',
+  host: 'localhost',
+  database: 'davidhodge',
+  password: '',
+  port: 5432
+});
+client.connect();
+
+// Middleware
 var requestTime = (req, res, next) => {
   req.requestTime = Date.now()
   console.log("Time: " + Date.now())
   next()
 }
 app.use(requestTime)
+
+const asyncMiddleware = fn =>
+(req, res, next) => {
+  Promise.resolve(fn(req, res, next))
+  .catch(next);
+};
 
 // function errorHandler (err, req, res, next) {
 //   if (res.headersSent) {
@@ -22,6 +41,18 @@ app.use(requestTime)
 app.get('/', (req, res) => {
   res.send({"value": "It's gucci two times!"})
 })
+
+app.get('/comments', (req, res, next) => {
+  client.query('SELECT NOW()', ['Fetched comments!'])
+  var rows = res.rows
+  if (rows == null || rows.length == 0) {
+    rows = []
+  }
+  res.send({
+    "results_count": 1,
+    "comments": rows
+  })
+});
 
 app.post('/test', (req, res) => {
   res.send({"that": "was a success"})
